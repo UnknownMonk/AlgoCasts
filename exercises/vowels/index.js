d1029c1b5aa9b55ed4bb405a4986a8d8ba133dca
UnknownMonk <smodcartoon@gmail.com>
2019-06-01 10:29:13 -0400

    vowels in a string

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowel = ['a', 'e', 'i', 'o', 'u'];
+  let numOfVowels = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (vowel.includes(char)) {
+      numOfVowels++;
+    }
+  }
+  return numOfVowels;
+}
+
+vowels('Happyday');
 
 module.exports = vowels;
